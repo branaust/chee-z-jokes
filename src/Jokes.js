747bcd54c1bc82a7c49154dbd7da066ff80ee46d
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-01-06 10:18:00 -0800

    init

---
+++ src/Jokes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Jokes extends Component {
+    render() {
+        return (
+            <div>JOKES!</div>
+        )
+    }
+}
+
+export default Jokes
